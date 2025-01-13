63d59ddffe81304d44daa0dba6dd20610b816ef7
koyookyung <rrrrr514@naver.com>
2025-01-13 17:46:59 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,100 @@
+$(function () {
+
+    //스크롤 시 배경색 & 헤더 변경
+    let sectionTop = $('.about').offset().top;
+    let footerTop = $('.contact').offset().top;
+    let navBar = $('.header .nav li');
+
+    //메뉴 클릭 시 스크롤 이동
+    $('.header .nav .gnb li').click(function (e) {
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+    });
+
+    //메뉴바 이동
+    let menuBar = $('.menu_bar');
+    let menu = $('.nav .gnb li');
+    let menuWidth = $('.nav .gnb li').outerWidth();
+
+    $('.menu_bar').css({
+        'width': menuWidth
+    });
+    $('.nav .gnb li').mouseenter(function () {
+        let moveLeft = $(this).position().left;
+        let moveWidth = $(this).outerWidth();
+
+        $('.menu_bar').stop().animate({
+            width: moveWidth,
+            left: moveLeft
+        });
+    });
+
+    $(window).scroll(function () {
+        let scrollBar = $(window).scrollTop();
+        console.log(scrollBar)
+
+        //스크롤 시 배경색 & 헤더 변경
+        if (scrollBar > sectionTop - 300) {
+            $('body').addClass('on')
+            $('.header').addClass('on');
+        } else {
+            $('body').removeClass('on')
+            $('.header').removeClass('on');
+        }
+        if (scrollBar > footerTop - 400) {
+            $('body').removeClass('on')
+            $('.header').addClass('active')
+        } else {
+            $('.header').removeClass('active')
+        }
+
+        //스크롤 시 메뉴바 이동
+        if ($('#intro').offset().top <= scrollBar) {
+            menuBar.stop().animate({
+                'width': menu.eq(0).outerWidth(),
+                'left': menu.eq(0).position().left
+            })
+        }
+        if ($('#about').offset().top - 50 <= scrollBar) {
+            menuBar.stop().animate({
+                'width': menu.eq(1).outerWidth(),
+                'left': menu.eq(1).position().left
+            })
+        }
+        if ($('#project').offset().top - 100 <= scrollBar) {
+            menuBar.stop().animate({
+                'width': menu.eq(2).outerWidth(),
+                'left': menu.eq(2).position().left
+            })
+        }
+        if ($('#design').offset().top - 150 <= scrollBar) {
+            menuBar.stop().animate({
+                'width': menu.eq(3).outerWidth(),
+                'left': menu.eq(3).position().left
+            })
+        }
+        if ($('#contact').offset().top - 150 <= scrollBar) {
+            menuBar.stop().animate({
+                'width': menu.eq(4).outerWidth(),
+                'left': menu.eq(4).position().left,
+            })
+            menuBar.css({
+                'background-color' : '#fff'
+            })
+        } else {
+            menuBar.css({
+                'background-color' : '#555'
+            })
+        }
+    });//scroll()
+
+    
+
+    //스킬 아이콘 호버 시 문장 나타남
+    let skillIcon = $('.skill .icon li');
+    let skillText = $('.skill .text li');
+
+    $(skillIcon).mouseenter(function () {
+        $(skillText).removeClass('on')
+        $(skillText).eq($(this).index()).addClass('on')
+    });
+});//script end
